cc80644962e9e5327544ea97105936ffb971115d
auburn <auburnete@gmail.com>
2021-04-08 23:30:06 +1000

    dummy commit for vercel

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,6 +1,7 @@
 // Snowpack Configuration File
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   optimize: {
