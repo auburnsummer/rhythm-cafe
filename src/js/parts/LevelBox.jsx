e163783b96c6f06f24a39556d4dde5af7201be42
auburnsummer <auburnete@gmail.com>
2021-04-11 05:19:53 +0000

    buying a gba sp

--- src/js/parts/LevelBox.jsx
+++ src/js/parts/LevelBox.jsx
@@ -10,15 +10,23 @@ export function LevelBox({level}) {
 		"Non-Referred: A trusted member of the community has checked for correct timing, metadata, and cues, and has found that the level does not meet standards."
 	]
 
+    const {thumb, artist, song, author} = level;
+
     return (
         <article class="lb">
             <div class="lb_imagebox">
-                <img class="lb_image" src={level.thumb} />
+                <img class="lb_image" src={thumb} />
             </div>
             <div class="lb_info">
-                <div>
-                    <h2 class="lb_artist">{level.artist}</h2>
-                    <h1 class="lb_song">{level.song}</h1>
+                <div class="lb_cast">
+                    <h2 class="lb_artist">{artist}</h2>
+                    <h1 class="lb_song">{song}</h1>
+                    <ul class="lb_metadata">
+                        <li class="lb_metaitem lb_author">
+                            <i class="lb_metaicon fad fa-pencil-alt"></i>
+                            <span class="lb_metatext">{author}</span>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </article>
