68b8363dc68ccb4060f481ae117de1ce3b91d273
auburn <auburnete@gmail.com>
2022-04-19 11:55:22 +1000

    mobile layout simple. until RD is a mobile game too there isn't much point putting too much effort into this tbh
    Former-commit-id: 2ed6466e9b609f2ce3720c1f9e20b7ba1d649b87 [formerly 3f4629eaed689bd438c181b4df75a07628d9aef8]
    Former-commit-id: 49c28d53ce9e22f724b591ea8979dfd9aab5cdee

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import "./App.css"
 import { Header } from "@orchard/components/Header"
 import { Levels } from "@orchard/components/Levels"
 import { Sidebar } from "@orchard/components/Sidebar"
+import { useState } from "preact/hooks"
+
+import cc from "clsx";
+import { Filter } from "./icons"
 
 export function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
     <div class="ap">
       <Header class="ap_header" />
+      <button class="ap_togglesidebar" onClick={() => setShowSidebar(prev => !prev)}>
+        <Filter class="ap_togglesidebaricon" />
+      </button>
       <div class="ap_layout">
-        <Sidebar class="ap_sidebar" />
+        <Sidebar class={cc("ap_sidebar", {"visible!ap_sidebar" : showSidebar})} />
         <Levels class="ap_levels" />
       </div>
     </div>
