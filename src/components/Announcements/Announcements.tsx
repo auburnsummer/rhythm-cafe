da1de3494fb662fa44652a42f952135cb26de1bc
willowpoke <109990491+willowpoke@users.noreply.github.com>
2024-12-06 01:34:44 -0500

    Update Announcements.tsx

--- src/components/Announcements/Announcements.tsx
+++ src/components/Announcements/Announcements.tsx
@@ -8,7 +8,7 @@ export function Announcements({ 'class': _class } : AnnouncementsProps) {
     return (
         <ul class={cc(_class, 'an')}>
             <li class="an_announcement"> {/* an announcement lol */}
-                Not sure where to start? Try the <a href="https://docs.google.com/spreadsheets/d/1acZltH8MKs81Nu-BOsaupeWfjJVDiDVoVzbIKepPdYQ/edit?usp=sharing">RDL Setlists!</a>
+                Not sure where to start? Try the <a href="https://docs.google.com/spreadsheets/d/1Uz26L34OZIgaK6hMfLBysgtEW4TYDj7iZZ5KqOMbNKY">RDL Setlists!</a>
             </li>
             <li class="an_announcement">
                 <a href="https://rd-editor-docs.github.io/intro/">Level editor tutorial</a> by Klyzx et al
